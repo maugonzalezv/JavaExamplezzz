48997704b7e79973f8d157c8eda1543a6eb3c39e
maugonzalezv <maugzzv2404@gmail.com>
2025-06-05 23:57:47 -0600

    act

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        int age =30;
+        age = age + 1;
+        System.out.println(age);
     }
 }
